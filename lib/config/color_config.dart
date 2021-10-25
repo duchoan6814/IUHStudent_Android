af0c6539596be31b8716ba2adff70713ec7a7c0f
Hoan Truong <hoantruong6814@gmail.com>
2021-10-25 21:52:46 +0700

    done build layout login screen

---
+++ lib/config/color_config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorConfig {
+  static const Color primary = Color(0xFF3EC3DB);
+  static const Color accent = Color(0xFFF26943);
+  static const Color warning = Color(0xFFEE5253);
+  static const Color orange = Color(0xFFF39F20);
+  static const Color green = Color(0xFF33C880);
+  static const Color tail = Color(0xFF01A3A4);
+  static const Color blue = Color(0xFF0075FF);
+  static const Color indigo = Color(0xFF6A3ACF);
+  static const Color grey = Color(0xFF8395A7);
+
+  static const Color textColor = Color(0xFF0C173C);
+  static const Color textColorLight = Color(0xB30C173C);
+
+  static const Color background = Color(0xFFF1F5FB);
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iuh_student/config/color_config.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,9 +7,71 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorConfig.background,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Image.asset("assets/images/Logo_IUH.png"),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0, 5)
+                )
+              ]
+            ),
+            child: Column(
+              children: [
+                const Text(
+                  "LOGIN",
+                  style: TextStyle(
+                    color: ColorConfig.textColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28.0
+                  ),
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: "USERNAME",
+                    labelStyle: TextStyle(
+                      color: ColorConfig.textColorLight,
+                      fontSize: 15.0
+                    )
+                  ),
+                ),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "PASSWORD",
+                    labelStyle: TextStyle(
+                      color: ColorConfig.textColorLight,
+                      fontSize: 15.0
+                    )
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(ColorConfig.primary),
+                    minimumSize: MaterialStateProperty.all(Size(double.infinity, 40.0)),
 
+                  ),
+                    onPressed: () => print("login"),
+                    child: Text("Login")
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
